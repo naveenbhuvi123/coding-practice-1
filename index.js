222e15618e8d00e89a5bc8e98991ec77c482d9c1
naveenbhuvi123 <bhuvinaveen@gmail.com>
2024-02-06 11:59:59 +0000

    coding-practice-1

---
+++ index.js
@@ -0,0 +1,9 @@
+const element = <div class="bg-container">
+        <h1 className="main-heading">Super Over league</h1>
+        <div className="image-container">
+            <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"  class="image" />
+            <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png"  class="image" />
+        </div>
+</div>;
+
+ReactDOM.render(element, document.getElementById("root"));
